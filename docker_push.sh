365914ca6617a8de95133eaf1bbb118bfffedc52
Matthew Tan <matthewtan90@gmail.com>
2020-05-03 17:13:49 +0100

    Update docker build and push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,12 +1,12 @@
 # Frontend
 docker build -f ./app_frontend/Dockerfile -t frontend ./app_frontend
-docker tag frontend:latest gcr.io/host-website-261608/frontend:latest
-docker push gcr.io/host-website-261608/frontend:latest
+docker tag frontend:latest gcr.io/matttan90-website/frontend:latest
+docker push gcr.io/matttan90-website/frontend:latest
 
 # Backend
 docker build -f ./app_backend/Dockerfile -t backend ./app_backend
-docker tag backend:latest gcr.io/host-website-261608/backend:latest
-docker push gcr.io/host-website-261608/backend:latest
+docker tag backend:latest gcr.io/matttan90-website/backend:latest
+docker push gcr.io/matttan90-website/backend:latest
 
 # Hot Redeploy
 gcloud container clusters get-credentials primary-cluster
